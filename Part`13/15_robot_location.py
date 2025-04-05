184667c6211e12ca0f708672c3606598942daac2
Santosh Narvekar <santynarvekar09@gmail.com>
2025-04-05 11:20:30 +0530

    Update 15_robot_location.py

--- Part`13/15_robot_location.py
+++ Part`13/15_robot_location.py
@@ -1,6 +1,6 @@
 # WRITE YOUR SOLUTION HERE:
 import pygame
-
+from random import randint
 pygame.init()
 window = pygame.display.set_mode((640, 480))
 
@@ -8,21 +8,27 @@ robot = pygame.image.load("robot.png")
 
 x = 0
 y = 0
+target_x = 0
+target_y = 0
 
 clock = pygame.time.Clock()
 
 while True: 
     for event in pygame.event.get():
-      if event.type == pygame.MOUSEMOTION:
-        x = event.pos[0]-robot.get_width()/2
-        y = event.pos[1]-robot.get_height()/2
+      if event.type == pygame.MOUSEBUTTONDOWN:
+        target_x = event.pos[0]
+        target_y = event.pos[1]
 
-        window.fill((0, 0, 0))
-        window.blit(robot,(x, y))
-        pygame.display.flip()
-    
+        if target_x > x and target_x < x+robot.get_width() and target_y > y and target_y < y+robot.get_width():
+           x = randint(0,window.get_width() - 50)
+           y = randint(0,window.get_height() - 86)
+   
       if event.type == pygame.QUIT:
          exit()
-
+    
+    window.fill((0, 0, 0)) 
+    window.blit(robot,(x, y))
+    pygame.display.flip()
+    
     clock.tick(60)
 
